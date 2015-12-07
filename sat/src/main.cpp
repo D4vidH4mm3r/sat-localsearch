ac8b52868333e8d53771b470c6ab6c0add0a3e55
David Hammer <d4vid.h4mm3r@gmail.com>
2015-12-07 13:50:07 +0100

    Running integer number of thread sets up to timeout

--- sat/src/main.cpp
+++ sat/src/main.cpp
@@ -20,6 +20,7 @@ int main(int argc, const char* argv[]) {
   string inputName = "../data/uf20-010.cnf";
   string outputName = "";
   int randSeed = 1;
+  double timeout = 20.0;
   {
     std::istringstream iss;
     for (int i=1; i<argc; i++) {
@@ -33,6 +34,10 @@ int main(int argc, const char* argv[]) {
       } else if (arg == "--main::seed") {
         iss.str(argv[i+1]);
         iss >> randSeed;
+        i++;
+      } else if (arg == "--main::timeout" || arg == "-t") {
+        iss.str(argv[i+1]);
+        iss >> timeout;
         i++;
       } else if (arg == "--verbose" || arg == "-v") {
         verbose = true;
@@ -65,19 +70,35 @@ int main(int argc, const char* argv[]) {
   randGen.seed(1);
   int numThreads = std::thread::hardware_concurrency();
   vector<std::future<SATState> > futures(numThreads);
-  for (int i=0; i<numThreads; i++) {
-    state.randomize();
-    futures[i] = std::async(std::launch::async, anneal, state, ref(randGen), verbose);
-  }
-  for (auto& future : futures) {
-    SATState res = future.get();
-    if (res.cost < bestState.cost) {
-      bestState = res;
+  int attempts = 0;
+  // TODO: better thread pooling?
+  while (true) {
+    for (int i=0; i<numThreads; i++) {
+      state.randomize();
+      futures[i] = std::async(std::launch::async, anneal, state, ref(randGen), verbose);
     }
+    for (auto& future : futures) {
+      SATState res = future.get();
+      if (res.cost < bestState.cost) {
+        bestState = res;
+      }
+    }
+    if (bestState.cost == 0) {
+      break;
+    }
+    // estimate whether we have time to try again
+    attempts++;
+    auto timeAfter = std::chrono::system_clock::now();
+    double timeSpent = static_cast<double>((timeAfter-timeBefore).count())/1e9;
+    double timePerAttempt = timeSpent/attempts;
+    if (timeSpent + 1.5*timePerAttempt > timeout) {
+      break;
+    }
+
   }
 
   auto timeAfter = std::chrono::system_clock::now();
-  std::chrono::nanoseconds timeSpent = timeAfter-timeBefore;
+  double timeSpent = static_cast<double>((timeAfter-timeBefore).count())/1e9;
   std::ofstream fileStream;
   if (outputName != "") {
     fileStream.open(outputName.c_str(), std::ios::out);
@@ -88,7 +109,7 @@ int main(int argc, const char* argv[]) {
   }
   std::ostream& outputStream = (outputName == "" ? cout : fileStream);
   outputStream << bestState;
-  outputStream << "Time: " << static_cast<double>(timeSpent.count())/1e9 << std::endl;
+  outputStream << "Time: " << timeSpent << std::endl;
   if (outputName != "") {
     fileStream.close();
   }
