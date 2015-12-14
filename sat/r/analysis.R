b74f2d6b8814458d2d47d9351df546659e7aa6a9
David Hammer <d4vid.h4mm3r@gmail.com>
2015-12-14 21:09:12 +0100

    More handy analysing + documenting (proof-of-concept)

--- sat/r/analysis.R
+++
@@ -1,8 +0,0 @@
-res <- read.table("../res/mincost-p-initial.csv", header=TRUE)
-res$instance <- factor(res$instance)
-res$p <- factor(res$p)
-res$class <- gsub("(u*f).*","\\1",res$instance)
-
-require(lattice)
-bwplot(reorder(p, cost)~cost|class, data=res, scales=list(relation="free"))
-bwplot(reorder(p, time)~time|class, data=(res[res$class=="uf",]), scales=list(relation="free"))
--- sat/scripts/benchmark.py
+++ sat/scripts/benchmark.py
@@ -3,17 +3,24 @@ import csv
 import sys
 
 
-print(sys.argv[1:])
-fn = (resdir / "mincost-p-initial.csv")
+if not resdir.exists():
+    resdir.mkdir()
+    
+fn = (resdir / "tune-p-unsatisfiable.csv")
 new = not fn.exists()
 with fn.open("a", newline="") as fd:
     csvwriter = csv.writer(fd, delimiter=" ", quotechar="\"")
     if new:
         csvwriter.writerow(["instance", "p", "cost", "time"])
     for instance in sorted(datadir.iterdir()):
+        if not instance.name.startswith("uuf250"):
+            print("Skipping", instance.name, "for now")
+            continue
+        print(instance.name)
         for p in sys.argv[1:]:
+            print(p)
             p = float(p)
-            res = runInstance(instance, t=3, p=p)
+            res = runInstance(instance, p=p, g=1)
             lines = res.split("\n")
             cost = int(lines[-3].split(" ")[-1])
             time = float(lines[-2].split(" ")[-1])
