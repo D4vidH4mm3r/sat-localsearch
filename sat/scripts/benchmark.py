ba1b51e3d62430344e0372d45f17133542cc75c2
David Hammer <d4vid.h4mm3r@gmail.com>
2015-12-15 12:22:20 +0100

    Working on testing + analysis of value for p

--- sat/scripts/benchmark.py
+++ sat/scripts/benchmark.py
@@ -1,26 +1,30 @@
 from common import *
+from argparse import ArgumentParser
 import csv
-import sys
 
+
+argparse = ArgumentParser(description="Run a series of tests")
+argparse.add_argument("output", help="Name of (csv) file to write to")
+argparse.add_argument("instanceclass", help="Prefix of name for test instances to use")
+argparse.add_argument("values", help="The parameter values to test", nargs="+", type=float)
+args = argparse.parse_args()
 
 if not resdir.exists():
     resdir.mkdir()
     
-fn = (resdir / "tune-p-unsatisfiable.csv")
+fn = (resdir / (args.output + ".csv"))
 new = not fn.exists()
 with fn.open("a", newline="") as fd:
     csvwriter = csv.writer(fd, delimiter=" ", quotechar="\"")
     if new:
         csvwriter.writerow(["instance", "p", "cost", "time"])
     for instance in sorted(datadir.iterdir()):
-        if not instance.name.startswith("uuf250"):
-            print("Skipping", instance.name, "for now")
+        if not instance.name.startswith(args.instanceclass):
             continue
         print(instance.name)
-        for p in sys.argv[1:]:
+        for p in args.values:
             print(p)
-            p = float(p)
-            res = runInstance(instance, p=p, g=1)
+            res = runInstance(instance, p=p)
             lines = res.split("\n")
             cost = int(lines[-3].split(" ")[-1])
             time = float(lines[-2].split(" ")[-1])
