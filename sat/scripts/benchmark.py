7f635056bf89b65c0ccd16b71967df5a2a390c50
David Hammer <d4vid.h4mm3r@gmail.com>
2015-12-15 16:27:58 +0100

    Making test-running script more general, improving reproduceability

--- sat/scripts/benchmark.py
+++ sat/scripts/benchmark.py
@@ -1,12 +1,13 @@
 from common import *
 from argparse import ArgumentParser
+from itertools import product
 import csv
 
 
 argparse = ArgumentParser(description="Run a series of tests")
 argparse.add_argument("output", help="Name of (csv) file to write to")
 argparse.add_argument("instanceclass", help="Prefix of name for test instances to use")
-argparse.add_argument("values", help="The parameter values to test", nargs="+", type=float)
+argparse.add_argument("-p", "--param", help="Parameters for search", action="append", nargs="+")
 args = argparse.parse_args()
 
 if not resdir.exists():
@@ -14,18 +15,21 @@ if not resdir.exists():
     
 fn = (resdir / (args.output + ".csv"))
 new = not fn.exists()
+paramNames = [p[0] for p in args.param]
+paramVals = [p[1:] for p in args.param]
 with fn.open("a", newline="") as fd:
     csvwriter = csv.writer(fd, delimiter=" ", quotechar="\"")
     if new:
-        csvwriter.writerow(["instance", "p", "cost", "time"])
+        csvwriter.writerow(["instance"] + paramNames + ["cost", "time"])
     for instance in sorted(datadir.iterdir()):
         if not instance.name.startswith(args.instanceclass):
             continue
         print(instance.name)
-        for p in args.values:
-            print(p)
-            res = runInstance(instance, p=p)
+        for comb in product(*paramVals):
+            kw = {name: val for name, val in zip(paramNames, comb)}
+            print(kw)
+            res = runInstance(instance, **kw)
             lines = res.split("\n")
             cost = int(lines[-3].split(" ")[-1])
             time = float(lines[-2].split(" ")[-1])
-            csvwriter.writerow([instance.name, p, cost, time])
+            csvwriter.writerow([instance.name] + list(comb) + [cost, time])
